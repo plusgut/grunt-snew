2208b6c1263642818a0310be001ad49ab0cab3da
plusgut <github@webentwickler2-0.de>
2015-06-27 18:10:00 +0200

    [grunt] added tempart parser

--- tasks/vood.js
+++ tasks/vood.js
@@ -1,6 +1,27 @@
+var tempart = require('tempart');
+
 module.exports = function(grunt) {
 	grunt.registerMultiTask('vood', 'Concatenate files.', function() {
 		// @TODO add sourcemap
-		console.log(this.files);
+		var target = this.target;
+		var container = '';
+		this.files.forEach(function(task) {
+			task.src.forEach(function(file) {
+				grunt.log.writeln('Working on: "' + file);
+
+				if(target == 'templates') {
+						console.log('template!');
+					var template = grunt.file.read(file);
+					var parsedTemplate = tempart.parse(template);
+					var containerTemplate = 'vood.Template({' + file + ', ' + JSON.stringify( parsedTemplate ) + '});';
+					container += containerTemplate + "\n";
+				} else {
+					console.log('nope');
+					container += grunt.file.read(file);
+				}
+			});
+			grunt.file.write(task.dest, container);
+		});
+
 	});
 };
