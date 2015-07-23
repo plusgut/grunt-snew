59e298e935cf818e5fb2cb42da181a63aaacfe37
plusgut <cj@lvl3.org>
2015-07-23 12:46:03 +0200

    [build] added module-layer

--- tasks/vood.js
+++ tasks/vood.js
@@ -2,22 +2,22 @@ var tempart = require('tempart');
 
 module.exports = function(grunt) {
 	grunt.registerMultiTask('vood', 'Concatenate files.', function() {
-		var lib = this.options().lib;
-		if(lib) {
+		var options = this.options();
+		if(options.lib) {
 			var vood = grunt.file.read(__dirname + '/../node_modules/vood/dist/vood.js');
-			grunt.file.write(lib, vood);
+			grunt.file.write(options.lib, vood);
 		} else {
 			grunt.fail.warn('The options object should contain a lib property with the path to dest/vood.js');
 		}
 
 		// @TODO add sourcemap
 		var target = this.target;
-		var container = '';
+		var container = "require(['vood/app'], function(vood) {window.vood = vood;\n";
 		this.files.forEach(function(task) {
 			task.src.forEach(function(file) {
 				grunt.log.writeln('Working on: ' + file);
 
-				if(target == 'templates') {
+				if(task.template) {
 					var template = grunt.file.read(task.cwd + '/' + file);
 					var parsedTemplate = tempart.parse(template);
 					var path = file.split('/');
@@ -27,10 +27,19 @@ module.exports = function(grunt) {
 				} else {
 					container += grunt.file.read(task.cwd + '/' + file);
 				}
+				container += "\n";
 			});
-
-			grunt.file.write(task.dest, container);
+			
+			
 		});
 
+		container += "\n});";
+
+		if(options.app) {
+			grunt.file.write(options.app, container);
+		} else {
+			grunt.fail.warn('The options object should contain a app property with the path to dest/app.js');
+		}
+		
 	});
 };
